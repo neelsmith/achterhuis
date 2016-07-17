49d20962c9e27adceffb30b02295b027498d9ec6
Neel Smith <dnsmith.neel@gmail.com>
2016-07-17 09:03:39 -0400

    Add error-checking assertion

--- scripts/ahparser.groovy
+++ scripts/ahparser.groovy
@@ -1,37 +1,50 @@
 /*
-Parses source text of Anne Frank document, and creates a tab-delimited file with
-two columns:
+Parses source text of Anne Frank document, and writes to standard out
+a tab-delimited file with two columns:
 
 1. a CTS URN for the passage
 2. the text contents of the passage
 
+Usage, from root of this repository: 
+
+    groovy scripts/ahparser.groovy srctext/achterhuis.txt
 
 */
 
 
-String urnNameSpace = "aflibre"
+// for error checking: count total number of daily diary entries:
+def expectedTotal = 165
+def matchCount = 0
+
+// read input from a file named on command line
+File f = new File(args[0])
+
+
+String urnNameSpace = "aflibre" // full URI == http://neelsmith.github.io/aflibre
 String workGroupId = "af" // Anne Frank
 String workId = "ah" // Achterhuis
 String version = "hc" // College of the Holy Cross
 String urnBase = "urn:cts:${urnNameSpace}:${urnNameSpace}.${workId}.${version}:"
 
-File f = new File(args[0])
-def mnthCountHash = ["Januari":"01", "Februari": "02", "Maart": "03",
-		     "April": "04", "Mei": "05", "Juni": "06",
-		     "Juli": "07", "Augustus": "08",  "September" : "09",
-		     "October": "10", "November": "11", "December": "12"]
 
+// map dutch month names to 2-char digit strings
+def mnthCountHash = [
+  "Januari":"01", "Februari": "02", "Maart": "03",
+  "April": "04", "Mei": "05", "Juni": "06",
+  "Juli": "07", "Augustus": "08",  "September" : "09",
+  "October": "10", "November": "11", "December": "12"
+]
 
-
+// current value of 8-char string for date
 String currDate = ""
+// current count of paragraph units in entry
 def pCount = 0
-def matchCount = 0
 f.eachLine { l ->
   def datematcher =  l =~ "(Maandag|Dinsdag|Woensdag|Donderdag|Vrijdag|Zaterdag|Zondag),+[ ]+([0-9]+)[ ]+([a-zA-Z]+)[ ]+(194[234])(.*)"
   if (datematcher.getCount()) {
     matchCount++;
     pCount = 0
-    datematcher.each { mtch, weekday,modate,mth,yr, rmdr ->
+    datematcher.each { wholematch, weekday, modate, mth, yr, remains ->
       if (modate.size() < 2) {
 	modate = "0${modate}"
       }
@@ -40,7 +53,7 @@ f.eachLine { l ->
     }
   } else {
     if (l ==~ /\s+/) {
-      
+      // skip blank or white-space-only lines
     } else {
       pCount++;
       println "${currDate}.p${pCount}\t${l}"
@@ -48,3 +61,4 @@ f.eachLine { l ->
   }
 }
 System.err.println "Total diary entries identified: " + matchCount
+assert matchCount == expectedTotal
